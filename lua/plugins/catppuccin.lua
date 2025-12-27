16f73eda0284cbe60ba650a4318266b5a4f18466
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-12-26 21:38:10 -0500

    Remove unneeded nested table

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,19 +1,17 @@
 return {
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        priority = 1000,
-        config = function()
-            require('catppuccin').setup({
-                flavour = 'mocha', -- latte, frappe, macchiato, mocha
-                transparent_background = true, -- disables setting the background color.
-                float = {
-                    transparent = false, -- enable transparent floating windows
-                    solid = true, -- use solid styling for floating windows, see |winborder|
-                },
-                auto_integrations = true,
-            })
-            vim.cmd.colorscheme 'catppuccin'
-        end,
-    },
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+        require('catppuccin').setup({
+            flavour = 'mocha', -- latte, frappe, macchiato, mocha
+            transparent_background = true, -- disables setting the background color.
+            float = {
+                transparent = false, -- enable transparent floating windows
+                solid = true, -- use solid styling for floating windows, see |winborder|
+            },
+            auto_integrations = true,
+        })
+        vim.cmd.colorscheme 'catppuccin'
+    end,
 }
