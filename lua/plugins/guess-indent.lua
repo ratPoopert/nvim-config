c26f44b82a998255b07b115180b126d657263d43
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-12-27 10:14:11 -0500

    Install GuessIndent

---
+++ lua/plugins/guess-indent.lua
@@ -0,0 +1,11 @@
+return {
+    'NMAC427/guess-indent.nvim',
+    opts = {
+        auto_cmd = true,
+        override_editorconfig = false,
+        filetype_exclude = {},
+        buftype_exclude = {},
+        on_tab_options = {},
+        on_space_options = {},
+    },
+}
